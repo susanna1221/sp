3ba8e3ce935171e548b748f72dd2187f6d6f8060
Jin Yu <susanna1221@naver.com>
2020-12-15 04:43:50 +0900

    some more tests

--- src/com/holub/database/Test.java
+++ src/com/holub/database/Test.java
@@ -31,6 +31,75 @@ public class Test {
 		
 		assertEquals(a.resultTable_csv.toString(), a.resultTable_xml.toString());
 		
+		
+		//And some more tests for Exporter, Importer
+		
+		File location = new File("C:\\dp2020");
+		File objectfile_addr = new File(location, "address.csv");
+		File objectfile_name= new File(location, "name.csv");
+		Path path_addr = objectfile_addr.getAbsoluteFile().toPath();
+		Path path_name = objectfile_name.getAbsoluteFile().toPath();
+		
+		/*CSV Importer*/
+		//address
+		BufferedReader br = null;
+		br = Files.newBufferedReader(path_addr);
+		Table t_csv_addr = new TableFactory().create(new CSVImporter(br));
+		br.close();
+		//name
+		br = Files.newBufferedReader(path_name);
+		Table t_csv_name = new TableFactory().create(new CSVImporter(br));
+		br.close();
+		
+		/*HTML Exporter*/
+		//address
+		Writer out = new FileWriter(new File(location, "address.html"));
+		t_csv_addr.export( new HTMLExporter(out) );
+		out.close();
+		//name
+		out = new FileWriter(new File(location, "name.html"));
+		t_csv_name.export( new HTMLExporter(out) );
+		out.close();
+		
+		/*XML Exporter*/
+		//address
+		out = new FileWriter(new File(location, "address.xml"));
+		t_csv_addr.export( new XMLExporter(out) );
+		out.close();
+		//name
+		out = new FileWriter(new File(location, "name.xml"));
+		t_csv_name.export( new XMLExporter(out) );
+		out.close();
+		
+		
+		
+		File objectfile_addr_xml = new File(location, "address.xml");
+		File objectfile_name_xml= new File(location, "name.xml");
+		Path path_addr_xml = objectfile_addr_xml.getAbsoluteFile().toPath();
+		Path path_name_xml = objectfile_name_xml.getAbsoluteFile().toPath();
+		
+		/*XML Importer*/
+		//address
+		br = Files.newBufferedReader(path_addr_xml);
+		Table t_addr_xml = new TableFactory().create(new XMLImporter(br));
+		br.close();
+		//name
+		br = Files.newBufferedReader(path_name_xml);
+		Table t_name_xml = new TableFactory().create(new XMLImporter(br));
+		br.close();
+		
+		assertEquals(t_csv_name.toString(), t_name_xml.toString());
+		
+		
+		/*CSV Exporter(XML -> CSV)*/
+		//address
+		out = new FileWriter(new File(location, "address_from_xml.csv"));
+		t_addr_xml.export( new CSVExporter(out) );
+		out.close();
+		//name
+		out = new FileWriter(new File(location, "name_from_xml.csv"));
+		t_name_xml.export( new CSVExporter(out) );
+		out.close();
 	
 
 	}
